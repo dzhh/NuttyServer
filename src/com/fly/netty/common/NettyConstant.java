d21c552b03b5b1b2ee3ae3eb07d6a7d5a3cfc770
DESKTOP-5HNJ83V\fly <dzhh2011@163.com>
2017-04-17 22:07:27 +0800

    no message

--- src/com/fly/netty/common/NettyConstant.java
+++ src/com/fly/netty/common/NettyConstant.java
@@ -7,7 +7,7 @@ package com.fly.netty.common;
  */
 public final class NettyConstant {
     public static final String REMOTEIP = "127.0.0.1";
-    public static final int PORT = 8080;
+    public static final int PORT = 10080;
     public static final int LOCAL_PORT = 12088;
     public static final String LOCALIP = "127.0.0.1";
 }
